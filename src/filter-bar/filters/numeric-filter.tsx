dfe0c9fcb682ff8a9ac49356a15a6f5bf6ddb205
Clint Andrews <clintandrews@stockx.com>
2019-04-01 11:28:14 -0400

    updated filters for single select and numeric .

---
+++ src/filter-bar/filters/numeric-filter.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { get, head, find, first } from 'lodash';
+import classNames from 'classnames';
+
+// Local imports
+import { FilterBars } from '../../index';
+import { Operations, Logics } from '../../enums';
+import { Dropdown } from '../../dropdown/dropdown';
+
+type StringFilterProps = {
+  showOperator?: boolean,
+  className?: string,
+  buttonClassName?: string,
+}
+
+const operations = [
+  {
+    operation: Operations.EQ,
+    label: 'Equal'
+  },
+  {
+    operation: Operations.LT,
+    label: 'Less than'
+  },
+  {
+    operation: Operations.GT,
+    label: 'More than',
+  },
+  {
+    operation: Operations.NEQ,
+    label: 'Not equal'
+  },
+  {
+    operation: Operations.GTE,
+    label: 'More or equal than',
+  },
+  {
+    operation: Operations.LTE,
+    label: 'Less or equal than',
+  },
+]
+
+export const getDefaultFilterQuery = <Tobj extends {}>(field: FilterBars.FitlerQueryField<Tobj>): FilterBars.FilterQuery<Tobj> => ({
+  field,
+  logic: Logics.OR,
+  filterItems: [{
+    operation: Operations.CONTAINS,
+    value: '',
+  }]
+});
+
+export class NumericFilter<Tobj> extends React.Component<FilterBars.FilterProps<Tobj, StringFilterProps>> {
+  public static defaultProps: FilterBars.IGetDefaultFilterQuery<unknown> = {
+    getDefaultFilterQuery,
+  }
+
+  onChangeValue: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.currentTarget.value;
+    const { onFilterUpdate, field } = this.props;
+
+    onFilterUpdate && onFilterUpdate({
+      field,
+      logic: Logics.OR,
+      filterItems: [{
+        operation: this.getOperation(this.props.filterQuery) || Operations.CONTAINS,
+        value: value,
+      }]
+    })
+  }
+
+  onChangeDropdown = (operation: Operations) => {
+    const { onFilterUpdate, field } = this.props;
+
+    onFilterUpdate && onFilterUpdate({
+      field,
+      logic: Logics.OR,
+      filterItems: [{
+        operation: operation,
+        value: this.getValue(this.props.filterQuery),
+      }]
+    })
+  }
+
+  getValue = (filterQuery: FilterBars.FilterQuery<Tobj> | undefined): any => {
+    return get(head(filterQuery && filterQuery.filterItems), 'value', '');
+  }
+
+  getOperation = (filterQuery: FilterBars.FilterQuery<Tobj> | undefined) => {
+    return get(head(filterQuery && filterQuery.filterItems), 'operation', Operations.CONTAINS);
+  }
+
+  render() {
+    const { field, filterQuery, className, label, showOperator, buttonClassName } = this.props;
+    const value = this.getValue(filterQuery);
+    const operation = this.getOperation(filterQuery);
+    const dropdownItems = operations.map(item => ({
+      option: item.label,
+      value: item.operation,
+    }));
+    const op = find(operations, i => i.operation === operation) || first(operations);
+
+    return (
+
+      <div className="filter-bar-input-group input-group">
+        {
+          showOperator &&
+          <div className="input-group-prepend">
+            <Dropdown items={dropdownItems} label={op ? op.label : ''} buttonClassName={buttonClassName} onChange={this.onChangeDropdown} />
+          </div>
+        }
+        <input className={classNames(className, 'filter-bar-input')} placeholder={label} type="number" name={field.toString()} value={value} onChange={this.onChangeValue} />
+      </div>
+    );
+  }
+}
---
+++ src/filter-bar/filters/select-filter.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { get, head, find, first, filter } from 'lodash';
+import classNames from 'classnames';
+import Select from 'react-select';
+
+
+// Local imports
+import { FilterBars } from '../../index';
+import { Operations, Logics } from '../../enums';
+import { ActionMeta, ValueType } from 'react-select/lib/types';
+import { Styles } from 'react-select/lib/styles';
+
+type MultiSelectFilterProps = {
+  options: Option[],
+  styles?: Partial<Styles>,
+  isMulti?: boolean
+}
+
+export type Option = {
+  value: string,
+  option: string,
+}
+
+export const getDefaultFilterQuery = <Tobj extends {}>(field: FilterBars.FitlerQueryField<Tobj>): FilterBars.FilterQuery<Tobj> => ({
+  field,
+  logic: Logics.OR,
+  filterItems: [],
+});
+
+export class SelectFilter<Tobj> extends React.PureComponent<FilterBars.FilterProps<Tobj, MultiSelectFilterProps>> {
+  public static defaultProps: FilterBars.IGetDefaultFilterQuery<unknown> = {
+    getDefaultFilterQuery,
+  }
+
+  onChangeValue: (values: ValueType<Option>, action: ActionMeta) => void = (values, action) => {
+    const { onFilterUpdate, field } = this.props;
+    const filterItems = (values && Array.isArray(values)) ? values.map(value => ({
+      operation: Operations.EQ,
+      value: value.value,
+    })) : [ {
+      operation: Operations.EQ,
+      value: values && values.value,
+    }];
+
+    onFilterUpdate && onFilterUpdate({
+      field,
+      logic: Logics.OR,
+      filterItems,
+    });
+  }
+
+  render() {
+    const { filterQuery, options, styles, isMulti  } = this.props;
+    const value = !!filterQuery ? filterQuery.filterItems.map(f => f.value as string) : [];
+
+    return (
+      <div>
+        <Select 
+          isMulti={isMulti}
+          styles={styles}
+          options={options}
+          getOptionLabel={o => o.option}
+          getOptionValue={o => o.value}
+          value={filter(options, (o: Option) => value.includes(o.value))}
+          onChange={this.onChangeValue}
+        />
+      </div>
+    );
+  }
+}
+
+export const customStyles = {
+  control: (styles: {}) => ({
+    ...styles,
+    minHeight: '34px',
+    minWidth: '10em',
+  }),
+  clearIndicator: (styles: {}) => ({
+    ...styles,
+    padding: '2px 8px',
+  }),
+  dropdownIndicator: (styles: {}) => ({
+    ...styles,
+    padding: '2px 8px',
+  }),
+  loadingIndicator: (styles: {}) => ({
+    ...styles,
+    padding: '2px 8px',
+  }),
+  menu: (styles: {}) => ({
+    ...styles,
+    zIndex: 3, // Without this menu will be overlapped by other fields
+  }),
+}
