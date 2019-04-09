08e7d9c59aa7d7bb8c05592f76ee6146aa179502
Clint Andrews <clintandrews@stockx.com>
2019-04-08 21:19:28 -0400

    fixes onFilterUpdate object reference

--- src/filter-bar/filter-bar.tsx
+++ src/filter-bar/filter-bar.tsx
@@ -88,7 +88,7 @@ export class FilterBar<Tobj> extends React.Component<FilterBarProps<Tobj>, Filte
 
         if (!!fql) {
             fql.filterQueries = [...fql.filterQueries, filterQuery];
-            onFilterUpdate(fql);
+            onFilterUpdate({...fql});
         }
     }
 
