6940d725b32e4eda65fe2f830e48af3ae5e50f53
Robert Chung <robertchung@Roberts-MacBook-Pro.local>
2019-04-12 15:39:41 -0400

    DropDownArrow

--- src/filter-bar-icons/index.ts
+++ src/filter-bar-icons/index.ts
@@ -1,11 +1,11 @@
-import { DropDownArrow } from './drop-down-arrow';
 import { DropDownX } from './drop-down-x';
 import { TimesCircle } from './times-circle';
+import { DropDownArrow } from './drop-down-arrow';
 
 import { IconLibrary } from 'react-svg-icon-host';
 
 export const FilterBarIcons: IconLibrary = {
-  DropDownArrow,
   DropDownX,
   TimesCircle,
+  DropDownArrow,
 };
